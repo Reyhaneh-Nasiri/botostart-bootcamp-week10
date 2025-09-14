07726e10f285a07771daaf5a8324c1c00b4ecafb
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-09-15 00:49:45 +0330

    feat(switch-mode): save theme mode in localStorage

--- assets/js/switchMode.js
+++ assets/js/switchMode.js
@@ -1,8 +1,17 @@
 const body = document.body;
 const switchModeBtn = document.querySelector(".switch-mode");
 
+if(localStorage.getItem("theme") === "dark"){
+  body.classList.add("dark-mode");
+}
+
 const themeHandler = () => {
   body.classList.toggle("dark-mode");
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
 };
 
 switchModeBtn.addEventListener("click", themeHandler);
