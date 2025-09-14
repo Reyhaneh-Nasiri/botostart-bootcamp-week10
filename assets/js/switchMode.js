10c8465649a0a861c535e67ede7093ff40ae6797
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-09-15 01:16:18 +0330

    feat(theme): detect system color scheme preference

--- assets/js/switchMode.js
+++ assets/js/switchMode.js
@@ -1,17 +1,29 @@
 const body = document.body;
 const switchModeBtn = document.querySelector(".switch-mode");
 
-if(localStorage.getItem("theme") === "dark"){
+if (
+  !localStorage.getItem("theme") &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  body.classList.add("dark-mode");
+  localStorage.setItem("theme", "dark");
+} else if (
+  !localStorage.getItem("theme") &&
+  !window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  body.classList.remove("dark-mode");
+  localStorage.setItem("theme", "light");
+}
+
+if (localStorage.getItem("theme") === "dark") {
   body.classList.add("dark-mode");
 }
 
 const themeHandler = () => {
   body.classList.toggle("dark-mode");
-  if (body.classList.contains("dark-mode")) {
-    localStorage.setItem("theme", "dark");
-  } else {
-    localStorage.setItem("theme", "light");
-  }
+  body.classList.contains("dark-mode")
+    ? localStorage.setItem("theme", "dark")
+    : localStorage.setItem("theme", "light");
 };
 
 switchModeBtn.addEventListener("click", themeHandler);
